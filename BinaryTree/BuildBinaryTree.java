4c5443ed9982a943447c42d60edab71715515567
Prakash <apps.prak@gmail.com>
2022-06-06 00:34:52 +0530

    Added a program to construct binary Tree

---
+++ BinaryTree/BuildBinaryTree.java
@@ -0,0 +1,14 @@
+package com.practice.binary;
+
+/*
+Given sequence:
+Inorder sequence: D B E A F C (Left,Root,Right
+Preorder sequence: A B D E C F
+
+
+
+
+ */
+
+public class BuildBinaryTree {
+}
