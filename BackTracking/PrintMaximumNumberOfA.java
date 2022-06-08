fd5f84cebf2e19c60095ddc247cdd92e1d795144
Prakash <apps.prak@gmail.com>
2022-06-08 11:43:51 +0530

    Tower of Hanoi problem

---
+++ BackTracking/PrintMaximumNumberOfA.java
@@ -0,0 +1,28 @@
+package com.backtracking;
+
+public class PrintMaximumNumberOfA {
+
+    static int findOptimal(int n){
+        if(n<6){
+            return n;
+        }
+
+        int max = 0;
+        int b;
+        for(b = n-3; b>=1; b--){
+            int curr = (n-b-1)*findOptimal(b);
+            if(curr > max){
+                max = curr;
+            }
+            return curr;
+        }
+        return max;
+    }
+
+    public static void main(String args[]){
+        System.out.println(findOptimal(3));
+        System.out.println(findOptimal(9));
+        System.out.println(findOptimal(7));
+
+    }
+}
--- BackTracking/ReadMe.txt
+++ BackTracking/ReadMe.txt
@@ -2,3 +2,4 @@ This folder will contains all the general problem of recursion and back tracking
 
 1. Given an array, remove adjacent duplicate character. RecursiveRemoveAdjacentDuplicateCharacter.java
 2. Tower of Hanoi. TowerOfHanoi.java
+3. Maximum number of A using four key [ Key1 -> 'A', Key2 -> 'Ctrl + A', Key3 -> 'Ctrl + B', Key4 -> 'Ctrl + V' . PrintMaximumNumberOfA.java  
