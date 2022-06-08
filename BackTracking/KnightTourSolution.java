a0acb48dbc55b9f3aa2d93d24b3cdb8905e12e47
Prakash <apps.prak@gmail.com>
2022-06-08 16:52:12 +0530

    Corrected the knight problem solution

--- BackTracking/KnightTourSolution.java
+++ BackTracking/KnightTourSolution.java
@@ -1,5 +1,12 @@
 package com.backtracking;
 
+
+/*
+
+This process prints all the possible solution.
+
+ */
+
 public class KnightTourSolution {
 
     public static void printKnightTour(int[][] chess, int r, int c, int move){
@@ -16,18 +23,19 @@ public class KnightTourSolution {
 
         printKnightTour(chess, r-2, c+1, move+1);
         printKnightTour(chess, r-1, c+2, move+1);
-        printKnightTour(chess, r+2, c+2, move+1);
+        printKnightTour(chess, r+1, c+2, move+1);
+        printKnightTour(chess, r+2, c+1, move+1);
         printKnightTour(chess, r+2, c-1, move+1);
         printKnightTour(chess, r+1, c-2, move+1);
         printKnightTour(chess, r-1, c-2, move+1);
         printKnightTour(chess, r-2, c-1, move+1);
-        printKnightTour(chess, r+1, c+2, move+1);
 
         chess[r][c] = 0;
 
     }
 
     public static void displayBorad(int[][] chess){
+        System.out.println("Solution: ");
         for(int i =0; i<chess.length ; i++){
             for(int j = 0; j<chess[0].length; j++){
                 System.out.print(chess[i][j] + " ");
@@ -37,19 +45,10 @@ public class KnightTourSolution {
     }
 
     public static void main(String [] args){
-        int [][] chess =
-                       {{0,0,0,0,0,0,0,0},
-                        {0,0,0,0,0,0,0,0},
-                        {0,0,0,0,0,0,0,0},
-                        {0,0,0,0,0,0,0,0},
-                        {0,0,0,0,0,0,0,0},
-                        {0,0,0,0,0,0,0,0},
-                        {0,0,0,0,0,0,0,0},
-                        {0,0,0,0,0,0,0,0}};
-        int r = 2;
-        int c = 3;
-
-        printKnightTour(chess, 8, 8, 0);
-
+        int n = 5;
+        int row = 0;
+        int column = 0;
+        int [][] chess = new int[n][n];
+        printKnightTour(chess, row,column, 1);
     }
 }
