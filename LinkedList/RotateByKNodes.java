0d31b774c97cf4c1561c61afc675e5e023536cd3
Prakash <apps.prak@gmail.com>
2022-06-13 00:33:53 +0530

    Rotate a given linked list by K nodes

---
+++ LinkedList/RotateByKNodes.java
@@ -0,0 +1,14 @@
+package com.linkedList;
+
+/*
+Rotate a given linked list counterclockwise by k nodes.
+
+Input:
+10 -> 20 -> 30 -> 40 -> 50 -> 60 -> null
+K = 4
+Output:
+50 -> 60 -> 10 -> 20 -> 30 -> 40 -> null
+
+ */
+public class RotateByKNodes {
+}
