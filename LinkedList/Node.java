82c36add9acebd0f4a3911fcbc0e2ca8225094b8
Prakash <apps.prak@gmail.com>
2022-06-27 20:43:26 +0530

    Detect a loop in linked list.

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -101,6 +101,19 @@ public class Node {
         return arr;
     }
 
+    public static Node getLinkedListWithLoop(){
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next = new Node(7);
+        head.next.next.next.next.next.next.next = new Node(8);
+        head.next.next.next.next.next.next.next.next = head.next.next.next.next;
+        return head;
+    }
+
 
 
 }
