be72a7574b48e266e34b421cabf28fab1d0e72cb
Prakash <apps.prak@gmail.com>
2022-07-01 11:36:21 +0530

    Sort hash map based on Values

---
+++ GeneralUtils/Utility.java
@@ -0,0 +1,13 @@
+package com.GeneralUtils;
+
+import java.util.Map;
+
+public class Utility {
+    public static void printMap(Map<String, Integer> map){
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println("Key : " + entry.getKey() + " Value : "+ entry.getValue());
+        }
+    }
+
+
+}
