272ef66789250b2f00da641238779577364be379
Prakash <apps.prak@gmail.com>
2022-07-01 15:37:35 +0530

    Corrected bubble sort code.

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -13,11 +13,11 @@ public class BubbleSort {
 
     public static void bubbleSort(int [] arr){
         int len = arr.length;
-        for(int i =0; i< len; i++){
-            for(int j = i +1; j< len; j++){
-                if(arr[i] > arr[j]){
+        for(int i =0; i< len -1 ; i++){
+            for(int j = 0 ; j< len -i -1; j++){
+                if(arr[j] > arr[j+1]){
                     // swap the numbers;
-                    ArrayHelperClass.swap(arr, i, j);
+                    ArrayHelperClass.swap(arr, j, j+1);
                 }
             }
         }
