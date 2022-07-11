58d814813b38f72a31decf9decf12687ed075753
Prakash <apps.prak@gmail.com>
2022-07-11 14:47:50 +0530

    Reverse an array using recursion

--- Recursion/ReverseAnArray.java
+++ Recursion/ReverseAnArray.java
@@ -3,15 +3,33 @@ package Recursion;
 public class ReverseAnArray {
 
 
+    public static void reverseAnArray(int [] arr, int i, int j){
+        if( i>=j){
+            return;
+        }else{
+            if(arr[i] != arr[j]){
+                swap(arr,i,j);
+            }
+        }
+        reverseAnArray(arr, i+1, j-1);
+    }
 
 
-
-    public static void swap(int [] arr){
-
+    public static void swap(int [] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
 
     public static void main(String [] args){
+        int arr[] = {1,2,3,4,5,6};
+
+        reverseAnArray(arr,0, arr.length -1);
+
+        for(int i = 0; i<=arr.length -1; i++){
+            System.out.print(arr[i] + " ");
+        }
 
     }
 }
