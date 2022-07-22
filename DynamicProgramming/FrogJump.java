b2312be60c505401bc42af1e939fc29c371f5d65
Prakash <apps.prak@gmail.com>
2022-07-22 08:01:54 +0530

    Tabulization Way

--- DynamicProgramming/FrogJump.java
+++ DynamicProgramming/FrogJump.java
@@ -14,6 +14,7 @@ public class FrogJump {
         Arrays.fill(dp,-1);
 
         System.out.println(memoizationWay(n, a,dp));
+        System.out.println(tebulizationWay(n, a));
     }
 
     static int recursiveWay(int n, int a[]){
@@ -27,16 +28,25 @@ public class FrogJump {
     static int memoizationWay(int n, int a[], int dp[]){
         if(n == 0) return 0;
         if(dp[n] != -1) return dp[n];
-        int left = recursiveWay(n-1, a) + Math.abs(a[n] - a[n-1]);
+        int left = memoizationWay(n-1, a, dp) + Math.abs(a[n] - a[n-1]);
         int right = Integer.MAX_VALUE;
-        if(n>1) right = recursiveWay(n-2, a) + Math.abs(a[n] - a[n-2]);
+        if(n>1) right = memoizationWay(n-2, a, dp) + Math.abs(a[n] - a[n-2]);
         dp[n] =  Integer.min(left,right);
         return dp[n];
 
     }
 
     static int tebulizationWay(int n, int a[]){
-        return 0;
+        int dp[] = new int[n];
+        Arrays.fill(dp,0);
+        dp[0] = 0;
+        for (int i =1; i<n;i++){
+            int fs = dp[i-1] + Math.abs(a[i] - a[i-1]);
+            int ss = Integer.MAX_VALUE;
+            if(i>1) ss = dp[i-2] + Math.abs(a[i] - a[i-2]);
+            dp[i] =  Integer.min(fs,ss);
+        }
+        return dp[n-1];
     }
 
 
