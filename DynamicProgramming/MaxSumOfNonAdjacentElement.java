2851ee19a38d69d2313e681720c20c43ce312c46
Prakash <apps.prak@gmail.com>
2022-07-23 10:51:42 +0530

    Structured the code.

--- DynamicProgramming/MaxSumOfNonAdjacentElement.java
+++ DynamicProgramming/MaxSumOfNonAdjacentElement.java
@@ -1,4 +1,23 @@
 package DynamicProgramming;
 
 public class MaxSumOfNonAdjacentElement {
+    public static void main(String args[]){
+        int arr[] = {1,3,2,4,8,3,9};
+
+        System.out.println("Max sum of non adjacent element in java: " + maxSumOfNonAdjacentElement(arr));
+    }
+
+    public static int maxSumOfNonAdjacentElement(int arr[]){
+
+        int maxSum = findMaxSum(0, arr);
+
+        return 0;
+    }
+
+
+    public static int findMaxSum(int index, int [] arr){
+
+
+        return 0;
+    }
 }
