0f121c958ed84e313f1fa5a0bf2b397d9cea184b
Prakash <apps.prak@gmail.com>
2022-08-01 13:12:36 +0530

    MaxMeeting number of meeting

---
+++ GreedyAlgo/MaxMeeting.java
@@ -0,0 +1,32 @@
+package GreedyAlgo;
+
+
+/**There is one meeting room in a firm. There are N meetings in the form of (S[i], F[i]) where
+ * S[i] is the start time of meeting i and F[i] is finish time of meeting i. The task is to find the maximum number of meetings
+ * that can be accommodated in the meeting room. Print all meeting numbers
+
+ * Input : s[] = {1, 3, 0, 5, 8, 5},
+ *         f[] = {2, 4, 6, 7, 9, 9}
+ * Output : 1 2 4 5
+ * First meeting [1, 2]
+ * Second meeting [3, 4]
+ * Fourth meeting [5, 7]
+ * Fifth meeting [8, 9]
+ *
+ * Input : s[] = {75250, 50074, 43659, 8931, 11273, 27545, 50879, 77924},
+ f[] = {112960, 114515, 81825, 93424, 54316, 35533, 73383, 160252 }
+ Output : 6 7 1
+ */
+
+public class MaxMeeting {
+
+    class Meeting{
+
+    }
+
+    public static void main(String [] args){
+        int s[] = {1, 3, 0, 5, 8, 5};
+        int f[] = {2, 4, 6, 7, 9, 9};
+
+    }
+}
