d3cebc3095ad4080439ae2b884a85161b98cfa9b
Prakash <apps.prak@gmail.com>
2022-08-11 02:02:13 +0530

    Updated the greater element code.

--- Array/LargestElementTotheRight.java
+++ Array/LargestElementTotheRight.java
@@ -1,5 +1,6 @@
 package array;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
@@ -7,7 +8,8 @@ import java.util.Stack;
 public class LargestElementTotheRight {
     public static void main(String args[]){
         int arr[] ={1,3,0,0,1,2,4 };
-        List<Integer> result = new LinkedList<>();
+        int finalResult[] = new int[arr.length];
+        List<Integer> result = new ArrayList<>();
         Stack<Integer> tempStore = new Stack<>();
 
         tempStore.push(arr[arr.length -1]);
@@ -25,9 +27,14 @@ public class LargestElementTotheRight {
         }
 
         if(result.size() != 0){
-            for(Integer item : result){
-                System.out.println(item);
+            int j =0;
+            for(int i = result.size() -1; i>=0;i--){
+                finalResult[j++] = result.get(i);
             }
+        }
+
+        for(int i =0; i<finalResult.length; i++){
+            System.out.println(finalResult[i]);
         }
 
 
